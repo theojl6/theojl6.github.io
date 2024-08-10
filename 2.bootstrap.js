a4d6d9c54c5e78a9696ac790d1a3ad06102923e6
theojl6 <leungteddy12@gmail.com>
2024-08-10 20:11:51 +0800

    update

--- 2.bootstrap.js
+++ 2.bootstrap.js
@@ -1,1 +1,1 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],[,function(e,n,t){"use strict";t.r(n);var o=t(3);const c=document.getElementById("code").value,u=o.a(c);document.getElementById("output").innerHTML=u}]]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],[,function(n,e,t){"use strict";t.r(e);var o=t(3);const c=document.getElementById("submit"),u=document.getElementById("code"),d=function(){document.getElementById("output").innerHTML=o.a(u.value)};c.onclick=d,u.onkeydown=function(n){"Enter"==n.key&&d()}}]]);
